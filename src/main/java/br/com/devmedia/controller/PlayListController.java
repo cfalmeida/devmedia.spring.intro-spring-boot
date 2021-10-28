843d0a49f45b770570d2b47f19dad65e45f75982
Cleber Almeida <cleberoficial10@gmail.com>
2021-10-27 22:05:55 -0300

    Adição na PlayListController dos métodos atualizar

--- src/main/java/br/com/devmedia/controller/PlayListController.java
+++ src/main/java/br/com/devmedia/controller/PlayListController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -28,10 +25,12 @@ public class PlayListController {
         model.addAttribute("playlists", playListService.recuperar());
         return new ModelAndView("/playlist/list", model);
     }
+
     @GetMapping("/cadastro")
     public String preSalvar(@ModelAttribute("playlist") PlayList playList){
         return "/playlist/add";
     }
+
     @PostMapping("/salvar")
     public String salvar(@Valid @ModelAttribute("playlist") PlayList playList,
                          BindingResult result, RedirectAttributes attr){
@@ -41,7 +40,23 @@ public class PlayListController {
         playListService.salvar(playList);
         attr.addFlashAttribute("mensagem", "PlayList criada com sucesso.");
         return "redirect:/playlists/listar";
+    }
 
+    @GetMapping("/{id}/atualizar")
+    public ModelAndView preAtualizar(@PathVariable("id") long id, ModelMap model){
+        PlayList playList = playListService.recuperarPorId(id);
+        model.addAttribute("playlist", playList);
+        return new ModelAndView("/playlist/add", model);
+    }
 
+    @PutMapping("/salvar")
+    public ModelAndView atualizar(@Valid @ModelAttribute("playlist") PlayList playList,
+                         BindingResult result, RedirectAttributes attr){
+        if(result.hasErrors())
+            return new ModelAndView("/playlist/add");
+
+        playListService.atualizar(playList);
+        attr.addFlashAttribute("mensagem", "PlayList atualizada com sucesso.");
+        return new ModelAndView("redirect:/playlists/listar");
     }
 }
